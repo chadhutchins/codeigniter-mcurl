d68aad6ce343bc560451b4e6c1cdae3b999e9721
Chad Hutchins <hutchins.chad@gmail.com>
2011-03-08 13:51:28 -0600

    initial commit

---
+++ application/controllers/example.php
@@ -0,0 +1,60 @@
+<?php
+
+class Example extends CI_Controller {
+    
+    function __construct()
+    {
+        parent::__construct();
+    }
+    
+    function index()
+    {
+        /*
+        ** Load the mcurl library
+        */
+        $this->load->library('mcurl');
+
+        /*
+        ** Example 1: Adding a Basic GET Request
+        */
+        $this->mcurl->add_call("get","http://chadhutchins.com");
+
+        /*
+        ** Example 2: Adding a Basic POST Request
+        */
+        $this->mcurl->add_call("post","http://twitter.com/chadhutchins");
+
+        /*
+        ** Example 3: Adding a more complex Request with Variables and Curl Options
+        */
+        $vars = array(
+            "v" => "1.0",
+            "q" => "blogHer"
+        );
+
+        $options = array(
+            CURLOPT_SSL_VERIFYPEER => FALSE
+        );
+
+        $this->mcurl->add_call("get","https://ajax.googleapis.com/ajax/services/search/blogs",$vars,$options);
+
+        /*
+        ** Example 4: Error Handling
+        */
+        $this->mcurl->add_call("post","httpasdf://twitter.com/chadhutchins");
+
+        /*
+        ** Execute the requests with Multiple Curl
+        */
+        $data = $this->mcurl->execute();
+
+        /*
+        ** Display the results with debug method
+        */
+        $this->mcurl->debug();
+        
+    }
+    
+}
+
+?>
